05374a35044e2a383a9a4bef3eae45dbd1b6adc0
peibanliubai <981841404@qq.com>
2017-12-13 21:34:23 +0800

    update

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+let path = require('path');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  entry: './src/main.js',//入口文件
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve('build')//上线打包文件
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.less$/,
+        use: ['style-loader', 'css-loader', 'less-loader']
+      },
+        {test:/\.(mp3|m4a|mp4)$/,loader:'file-loader'},
+
+        {
+        test: /\.(jpg|png|gif)/,
+        use: 'url-loader'
+      }
+    ]
+  },
+    //配置调试工具，报错的话会提示源码位置
+  devtool: 'cheap-module-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
+  ]
+} 
