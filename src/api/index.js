8b50ea4aaa915e130734c5acd31254c0fcc14b2f
limin0428 <1690324671@qq.com>
2017-12-19 11:36:57 +0800

    limin

--- src/api/index.js
+++ src/api/index.js
@@ -48,33 +48,7 @@ export function post(url,id) {
             body: JSON.stringify({'id':id})
         }).then((response) => {
              console.log(response.json());
-        }).then((json) => {
-                let hasError = false;
-                let text = '';
-                /// /api/list正常返回格式{errcode:0,errmsg:'',data:[]}
-                if (json.errcode !== 0) {
-                    hasError = true;
-                    text = json.errmsg;
-                } else {
-                    hasError = false;
-                    text = '';
-                }
-                // 网络异常 如断网
-                if (json.error) {
-                    dispatch(success({
-                        msg: strings.NET_ERROR,
-                        data: false
-                    }));
-                }
-                // 正常业务处理
-                else {
-                    dispatch(success({
-                        msg: hasError ? text : '',
-                        data: !hasError
-                    }));
-                }
-            })
-            .catch((err) => {
+        }).catch((err) => {
                 dispatch(success({
                     msg: (__DEBUG__ && err.message) ? err.message : strings.NET_ERROR,
                     data: false
