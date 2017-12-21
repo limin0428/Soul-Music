998616b19bf91b49b3c074d99f6a3970baba9f36
limin0428 <1690324671@qq.com>
2017-12-21 18:01:30 +0800

    limin

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -7,7 +7,7 @@ export default class Home extends Component{
     render(){
         return (
             <div>
-              <HomeTab/>
+              {/*<HomeTab/>*/}
               <HomeFooter/>
             </div>
         )
