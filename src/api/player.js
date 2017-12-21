ca9c09fe541f102bd9a003d934ce51c2b2b789fc
limin0428 <1690324671@qq.com>
2017-12-21 18:07:18 +0800

    limin

--- src/api/player.js
+++
@@ -1,8 +0,0 @@
-import {get, post} from './index';
-//获取歌曲列表
-
-
-export function getPlayList(id) {
-    return post('/music',id);
-}
-getPlayList(467668);
--- src/store/reducers/session.js
+++
@@ -1,6 +0,0 @@
-import * as types from '../action-types';
-let initState={
-    error:'',
-    success:'',
-    user:null
-}
