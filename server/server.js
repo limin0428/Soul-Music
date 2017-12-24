fdd2a66209856d171ef67e282a7081c4da64f9fc
peibanliubai <981841404@qq.com>
2017-12-24 11:33:36 +0800

    调试

--- server/server.js
+++ server/server.js
@@ -6,14 +6,6 @@ app.use(bodyParser.json());
 let session = require('express-session');
 app.listen(9527);   // 后台端口
 let listen = 3000;  // 数据端口
-// let sliders=require('./mock/sliders');
-// let recommendList=require('./mock/recommendList');
-// let newSongs=require('./mock/newSongs');
-// let searchRes=require('./mock/searchResult');
-// let track=require('./mock/track');
-// let rankList=require('./mock/rankList');
-// let playlist=require('./mock/playlist');
-// let songs=require('./mock/songs');
 
 
 app.use(session({
@@ -139,11 +131,13 @@ app.get('/mvinfo', (req, res) => {
         res.json(JSON.parse(data));
     });
 });
+//获取单曲
 app.get('/music',(req,res) => {
     request(`http://localhost:${listen}/music/url?id=${req.query.id}`, function (error, response, data) {
         res.json(JSON.parse(data));
     })
 });
+//获取歌手
 app.get('/musicsingle',(req,res) => {
     request(`http://localhost:${listen}/artists?id=${req.query.id}`, function (error, response, data) {
         res.json(JSON.parse(data));
@@ -154,24 +148,5 @@ app.get('/musicsingle',(req,res) => {
 
 
 
-
-/*
-let {type="",offset=0,limit=5} = req.query;
-offset = isNaN(offset)?0:parseInt(offset);
-limit = isNaN(limit)?0:parseInt(limit);
-let newLessons = JSON.parse(JSON.stringify(lessons));
-//如果type为空，则不过滤课程类型，如果type不为空，则只出现课程类型跟传入的type相同的课程
-newLessons.list = newLessons.list.filter(item=>item.type == type || type =="");
-// 0+5  5+5=10 10+5=15 15+5=20
-//如果下一页的起始索引已经大于等于总条数了，则认为已经分页完毕，后面已经没有数据了
-newLessons.hasMore = limit+offset<newLessons.list.length;//20
-//提取指定页的数据
-newLessons.list = newLessons.list.slice(offset,offset+limit);//offset0 0-4
-for(let i=0;i<newLessons.list.length;i++){
-    let lesson = newLessons.list[i];
-    lesson.title = `${offset+i+1}-${lesson.title}`;
-}
-res.json(newLessons);
-*/
 
 
